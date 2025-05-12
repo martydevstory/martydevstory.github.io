3ce7cb44f4bb09f61790932a0ce3bb5251355698
martydevstory <martydevstory@users.noreply.github.com>
2025-05-12 03:14:12 +0000

    deploy: 785f204d98fb324ffc5920d418b3fa62c8f82921

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746628437',resources: [
+    cacheName: 'chirpy-1747019651',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
